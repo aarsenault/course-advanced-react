39ce2708a02e8bff8e9a31702e7e1251ac938bc8
Adriel Arsenault <adriel.arsenault@gmail.com>
2019-10-11 11:12:36 -0400

    first push old machine

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -2,6 +2,8 @@
 // ? wonder if it's somehow using parcel.
 // import React from 'react';
 
+import Link from 'next/link';
+
 // ? class component
 // class Home extends React.Component {
 // 	render () {
@@ -9,6 +11,13 @@
 // 	}
 // }
 
-const Home = props => <div> Hey! </div>;
+const Home = props => (
+  <div>
+    <p>Hey!</p>
+    <Link href="/sell">
+      <a>Sell!</a>
+    </Link>
+  </div>
+);
 
 export default Home;
