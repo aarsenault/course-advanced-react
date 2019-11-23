e963c5d9c77f9f44e2d78ebd9e6623e0b392ef86
Adriel Arsenault <adriel.arsenault@gmail.com>
2019-11-23 08:51:34 -0500

    push up recent

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+// this is a js object, not CSS!
+const theme = {
+  red: '#ff0000',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightgrey: '#e1e1e1',
+  offWhite: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
         {this.props.children}
-      </div>
+      </StyledPage>
     );
   }
 }
